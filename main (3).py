3a15771e71212716630175db405f9e57efe0ad4f
Alexjapal <112505792+Alexjapal@users.noreply.github.com>
2022-09-01 00:26:40 +0100

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,15 @@
+import turtle
+kevin = turtle.Turtle()
+kevin.color("blue")
+kevin.speed(3)
+for e in range(10):
+  kevin.begin_fill()
+  kevin.forward(100)
+  kevin.right(50)
+  kevin.forward(100)
+  kevin.right(130)
+  kevin.forward(100)
+  kevin.right(50)
+  kevin.forward(100)
+  kevin.right(95)
+kevin.end_fill
